ae52240449115a80d1a077c9d1517e0879ddb984
intvirtue <intvirtue@gmail.com>
2023-02-12 05:23:56 +0400

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,7 +7,7 @@ const ALCHEMY_API_KEY = "5VhTy4aErPFixkOXQaCL0yhGjm9pknIZ";
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Beware: NEVER put real Ether into testing accounts
-const GOERLI_PRIVATE_KEY = "18f9328338f4f34dcef007d5de2fc59cbba8d3336453e1f06ad0026fd866ee2f";
+const GOERLI_PRIVATE_KEY = "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
@@ -20,7 +20,7 @@ const config: HardhatUserConfig = {
   etherscan: {
     // Your API key for Etherscan
     // Obtain one at https://etherscan.io/
-    apiKey: "CV9XD8MZDB74YHRV8R25K8TV272QW4AAV2"
+    apiKey: ""
   },
 };
 
